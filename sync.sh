0989c9f425f904e147e65bd748186105d768fa2a
Dobrica <hpdobrica@gmail.com>
2022-09-25 16:23:14 +0200

    test alert

--- sync.sh
+++ sync.sh
@@ -23,21 +23,23 @@ check_for_conflicts
 MSG="Pixel 6 backup at $(date)"
 
 git submodule foreach git add -A .
-git submodule foreach git commit -am "$MSG"
+git submodule foreach "git commit -am '$MSG' || true"
 git submodule foreach git pull --rebase origin main
 check_for_conflicts
 git submodule foreach git push
 
 
 git add -A
-git commit -am "$MSG"
+git commit -am "$MSG" || true
 git pull --rebase origin main
 check_for_conflicts
 git push
 
+
 if [ -z "$(git status --porcelain)" ]; then
   echo "working directory clean, all good!"
 else
+  echo "working directory not clean, script has likely failed to perform sync"
   termux-notification -c "obsidian sync finished, but working directory is not clean - something might be wrong"
 
 fi
